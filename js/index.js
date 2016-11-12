3461481b0f50c41ea2794b4e3962f4e2f046291d
NaughtAleph <addisonfattor@yahoo.com>
2016-11-12 13:42:53 -0800

    deleted

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$("#login").click(function(){
-	window.location.href = "login.html"
-});
-
-$.get('php/check_session.php', function(data) {
-	if (data == "no") {
-		$("#errormessage").html("Invalid session key");
-	}
-});
